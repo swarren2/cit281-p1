fcb1b5965c983f5f6ac9bf4f53d48460157cd39a
swarren2 <77074432+swarren2@users.noreply.github.com>
2021-06-03 16:26:38 -0700

    Create p1-date.js

---
+++ p1-date.js
@@ -0,0 +1,9 @@
+/*
+    CIT 281 Project 1
+    Name: Sydnee Warren
+*/
+
+//day of week
+let x = new Date();
+let y = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+console.log(y[x.getDay()]);
